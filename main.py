d2d9b8a98b7a66f2daa05b0ff70f16eb6ff9fb3d
yasith <yasith@cs.odu.edu>
2020-04-25 17:47:15 -0400

    added capsconv implementation (partial)

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def create_capsnet_model(input_shape, name) -> k.Model:
     # initial convolution
     l2 = k.layers.Conv2D(filters=256, kernel_size=(9, 9), strides=(1, 1), activation='relu', name='conv')(l1)  # type: tf.Tensor
     # convolution capsule layer
-    l3 = CapsConv2D(caps_layers=32, caps_dims=8, kernel_size=(9, 9), strides=(2, 2), activation='relu', name='conv_caps')(l2)  # type: tf.Tensor
+    l3 = CapsConv2D(caps_filters=32, caps_dims=8, kernel_size=(9, 9), strides=(2, 2), activation='relu', name='conv_caps')(l2)  # type: tf.Tensor
     # dense capsule layer with dynamic routing
     l4 = CapsDense(caps=10, caps_dims=16, routing_iter=3, name='dense_caps')(l3)  # type: tf.Tensor
     # decoder
