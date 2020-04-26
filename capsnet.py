5b1c4fd8e8c2d79f92ec552ab7bf0915480a3c04
yasith <yasith@cs.odu.edu>
2020-04-26 00:10:14 -0400

    add 3d convcaps implementation

--- capsnet.py
+++ capsnet.py
@@ -1,6 +1,9 @@
+from typing import Tuple
+
 import tensorflow as tf
 from tensorflow import keras as k
 from tensorflow_core.python.keras.utils import conv_utils
+from tensorflow_core.python.layers.base import InputSpec
 
 
 class Activations:
@@ -76,36 +79,100 @@ class CapsConv2D(k.layers.Conv2D):
 
 
 class CapsConv(k.layers.Conv3D):
-    def __init__(self, caps_layers, caps_dims, routing_iter, kernel_size, strides, **kwargs):
-        self.caps_layers = caps_layers
+    def __init__(self, caps_filters, caps_dims, routing_iter, kernel_size, strides, **kwargs):
+        self.caps_filters = caps_filters
         self.caps_dims = caps_dims
         self.routing_iter = routing_iter
+        # build-time parameters
+        self.input_caps_filters = ...
+        self.input_caps_dims = ...
+        self.conv_input_shape = ...
         self.w = ...
         # only accept kernel_size and strides specified in 2D
         kernel_size = conv_utils.normalize_tuple(kernel_size, 2, name='kernel_size')
         strides = conv_utils.normalize_tuple(strides, 2, name='strides')
-        super(CapsConv, self).__init__(self.caps_layers * self.caps_dims, (*kernel_size, 1), strides=(*strides, 1), **kwargs)
+        # call super init function
+        super(CapsConv, self).__init__(self.caps_filters * self.caps_dims, (*kernel_size, 1), strides=(*strides, 1), **kwargs)
 
     def build(self, input_shape: tf.TensorShape):
-        assert input_shape.rank == 5
-        input_caps_dims = input_shape[4]
+        # sanity check
+        tf.assert_equal(input_shape.rank, 5, message=f'Expected Tensor of Rank = 5, Got Rank = {input_shape.rank}')
+        # define capsule parameters
+        self.input_caps_filters = input_shape[3]
+        self.input_caps_dims = input_shape[4]
         # configure kernel_size and stride for capsule-wise convolution in the last dimension
-        self.kernel_size = (*self.kernel_size[:-1], input_caps_dims)
-        self.strides = (*self.strides[:-1], input_caps_dims)
-        super(CapsConv, self).build(input_shape)
+        self.kernel_size = (*self.kernel_size[:-1], self.input_caps_dims)
+        self.strides = (*self.strides[:-1], self.input_caps_dims)
+        # call super build function. pass input shape for 3D convolution here
+        super(CapsConv, self).build((*input_shape[:3], self.input_caps_filters * self.input_caps_dims, 1))
+        # update input spec to match original rank
+        self.input_spec = InputSpec(ndim=5)
 
     def call(self, inputs, **kwargs):
-        # reshape (..., caps_layers, caps_dims) for depth-wise convolution
-        new_shape = (*inputs.shape[:-2], inputs.shape[-2] * inputs.shape[-1], 1)
-        inputs = tf.reshape(inputs, shape=new_shape)  # shape: (batch_size, rows, cols, input_caps_layers * input_caps_dims, 1)
+        # reshape (..., caps_filters, caps_dims) for depth-wise convolution
+        new_shape = (-1, *inputs.shape[1:3], self.input_caps_filters * self.input_caps_dims, 1)
+        inputs = tf.reshape(inputs, shape=new_shape)  # shape: (..., input_caps_filters * input_caps_dims, 1)
         # perform 3D convolution
-        result = super(CapsConv, self).call(inputs)  # shape: (batch_size, new_rows, new_cols, input_caps_layers, caps_layers * caps_dims)
-        # transpose and reshape
-        result = tf.linalg.matrix_transpose(result)  # shape: (batch_size, new_rows, new_cols, caps_layers * caps_dims, input_caps_layers)
-        new_shape = (-1, result.shape[1], result.shape[2], result.shape[3] // self.caps_dims, self.caps_dims, result.shape[4])
-        result = tf.reshape(result, shape=new_shape)  # shape: (batch_size, new_rows, new_cols, caps_layers, caps_dims, input_caps_layers)
+        result = super(CapsConv, self).call(inputs)  # shape: (batch_size, new_rows, new_cols, input_caps_filters, caps_filters * caps_dims)
+        # reshape into (..., caps_filters, caps_dims)
+        result = tf.reshape(result, shape=(-1, *result.shape[1:4], self.caps_filters, self.caps_dims))
         # dynamic routing
-        return Activations.squash(result, axis=-1)
+        prediction = CapsConv.dynamic_routing(
+            routing_iter=self.routing_iter,
+            caps_filters=self.caps_filters,
+            input_caps_filters=self.input_caps_filters,
+            prediction=result
+        )
+        # return result
+        return prediction
+
+    @staticmethod
+    @tf.function
+    def dynamic_routing(routing_iter, caps_filters, input_caps_filters, prediction):
+        """
+        Dynamic routing
+
+        :param input_caps_filters:
+        :param caps_filters:
+        :param routing_iter: number of routing iterations, >=1
+        :param prediction: shape: (batch_size), (rows, cols, input_caps_filters, caps_filters, caps_dims)
+        :return:
+        """
+        # sanity check
+        tf.assert_greater(routing_iter, 0, message='Dynamic Routing should have >= 1 iterations')
+        # get batch size
+        batch_size = tf.shape(prediction)[0]
+        # define routing weight - shape: (batch_size), (rows, cols, caps_filters, input_caps_filters)
+        w = tf.zeros(shape=(batch_size, *prediction.shape[1:3], caps_filters, input_caps_filters))
+        # placeholder for routed prediction
+        routed_prediction = ...
+        # routing
+        for _ in range(routing_iter):
+            # calculate coupling coefficient
+            cc = CapsConv.softmax(w, axis=(1, 2, 3))  # shape: (batch_size), (rows, cols, caps_filters, input_caps_filters)
+            # calculate routed prediction
+            routed_prediction = tf.einsum('pqrst,pqrtsu->pqrsu', cc, prediction)
+            # squash over caps_dims
+            routed_prediction = Activations.squash(routed_prediction, axis=-1)  # shape: (batch_size), (rows, cols, caps_filters, caps_dims)
+            # calculate agreement between original prediction and routed prediction
+            agreement = tf.einsum('pqrst,pqrust->pqrsu', routed_prediction, prediction)
+            # update routing weight
+            w = w + agreement
+        # return routed prediction
+        return routed_prediction
+
+    @staticmethod
+    @tf.function
+    def softmax(data: tf.Tensor, axis: Tuple) -> tf.Tensor:
+        """
+        softmax over multiple dimensions
+        :param axis: axes over which to reduce
+        :param data: input tensor
+        """
+        tf.assert_equal(len(axis), 3, message=f'Expected 3 dimensions to calculate softmax on, got {len(axis)} instead.')
+        data_exp = tf.exp(data)
+        data_exp_sum = tf.reduce_sum(data_exp, axis=axis, keepdims=True)
+        return data_exp / data_exp_sum
 
 
 class CapsDense(k.layers.Layer):
@@ -130,9 +197,12 @@ class CapsDense(k.layers.Layer):
         return config
 
     def build(self, input_shape: tf.TensorShape):
-        assert input_shape.rank == 5
+        # sanity check
+        tf.assert_equal(input_shape.rank, 5, message=f'Expected Tensor of Rank = 5, Got Rank = {input_shape.rank}')
+        # define capsule parameters
         self.input_caps = input_shape[1] * input_shape[2] * input_shape[3]
         self.input_caps_dims = input_shape[4]
+        # define weights
         self.w = self.add_weight(
             name='w',
             shape=(1, self.input_caps, self.caps, self.caps_dims, self.input_caps_dims),
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 from tensorflow import keras as k
 from tensorflow.keras.datasets import mnist
 
-from capsnet import CapsConv2D, CapsDense, Losses, Metrics
+from capsnet import CapsConv2D, CapsDense, Losses, Metrics, CapsConv
 
 # Set random seeds so that the same outputs are generated always
 np.random.seed(42)
@@ -37,8 +37,10 @@ def create_capsnet_model(input_shape, name) -> k.Model:
     l1 = k.layers.Input(shape=input_shape, name='input')  # type: tf.Tensor
     # initial convolution
     l2 = k.layers.Conv2D(filters=256, kernel_size=(9, 9), strides=(1, 1), activation='relu', name='conv')(l1)  # type: tf.Tensor
-    # convolution capsule layer
-    l3 = CapsConv2D(caps_filters=32, caps_dims=8, kernel_size=(9, 9), strides=(2, 2), activation='relu', name='conv_caps')(l2)  # type: tf.Tensor
+    # layer to convert to capsule domain
+    l3 = CapsConv2D(caps_filters=32, caps_dims=8, kernel_size=(9, 9), strides=(2, 2), activation='relu', name='conv_caps_2d')(l2)  # type: tf.Tensor
+    # conv capsule layer with dynamic routing
+    l3 = CapsConv(caps_filters=32, caps_dims=8, kernel_size=(2, 2), strides=(1, 1), routing_iter=3, name='conv_caps_3d')(l3)  # type: tf.Tensor
     # dense capsule layer with dynamic routing
     l4 = CapsDense(caps=10, caps_dims=16, routing_iter=3, name='dense_caps')(l3)  # type: tf.Tensor
     # decoder
